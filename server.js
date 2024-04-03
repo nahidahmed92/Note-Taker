6740993a07e484b6840e1755450e276b6d2cbedb
Nahid Ahmed <npahmed92@gmail.com>
2024-04-03 00:49:13 -0400

    add section headers

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+// DEPENDENCIES ======================================
 const express = require('express');
+const path = require('path');
+
+// DATA ==============================================
+
+// APP/PORT ==========================================
 const app = express();
 const port = process.env.PORT || 3000;
 
+// MIDDLEWARES =======================================
+
+// ROUTES ============================================
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// START THE SERVER ==================================
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
