21d542d8c559f6e38289a3ddddcd9d8b8db48259
Nahid Ahmed <npahmed92@gmail.com>
2024-04-03 01:28:44 -0400

    add notes route

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,8 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+module.exports = notes;
