eb6c6f686459aa7e8e480811bdd33d884cd6dfe4
Nahid Ahmed <npahmed92@gmail.com>
2024-04-04 18:44:44 -0400

    add notes to routes index

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-
 // Import modular routers for /notes
 const notesRouter = require('./notes.js');
 
+// call notes router
 router.use('/notes', notesRouter);
 
 module.exports = router;
