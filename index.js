25f8141e16b8ca614468923e80405ebbb8111a4c
Nahid Ahmed <npahmed92@gmail.com>
2024-08-26 15:34:17 -0400

    update cors

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 // DEPENDENCIES ======================================
+const cors = require('cors');
 const express = require('express');
 const path = require('path');
+
 const api = require('./routes/index.js');
 
 // DATA ==============================================
+const corsOptions = {
+  origin: 'https://note-taker.nahidahmed.com',
+  methods: ['GET', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true,
+};
 
 // APP/PORT ==========================================
 const app = express();
 const port = process.env.PORT || 3001;
 
 // MIDDLEWARES =======================================
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
