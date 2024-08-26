bfee6374fb4eeee622073a85f536fb418707cd26
Nahid Ahmed <npahmed92@gmail.com>
2024-08-26 18:03:38 -0400

    use relative path for public folder

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // ROUTES ============================================
 // GET Route for homepage
